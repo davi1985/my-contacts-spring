d745dbeffff3744424fb31f79f615fe4e3a353d8
Davi Silva <davisilvaphoto@gmail.com>
2025-05-26 09:12:46 -0300

    api endpoint test

---
+++ src/main/java/com/sarahcode/contacts/api/controllers/CategoryController.java
@@ -0,0 +1,15 @@
+package com.sarahcode.contacts.api.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/categories")
+public class CategoryController {
+
+    @GetMapping
+    public String getCategories() {
+        return "List of categories";
+    }
+}
